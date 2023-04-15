c0168ce5e55f54e839eaef8a031f8a950c70284e
李松 <lisong23366@gmail.com>
2023-04-15 22:06:03 +0800

    mybatisplus的demo

---
+++ demo-mybatisplus/src/main/java/cn/itcast/demomybatisplus/DemoMybatisplusApplication.java
@@ -0,0 +1,13 @@
+package cn.itcast.demomybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMybatisplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMybatisplusApplication.class, args);
+    }
+
+}
